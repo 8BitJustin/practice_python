c7603fa9da3145b6aa9a34e950cabca3eacd100c
8bitjustin <j.olson.digital@gmail.com>
2019-09-26 07:20:29 -0700

    Added extra code to 03, regarding user input.

--- 03_list_less_than_ten.py
+++ 03_list_less_than_ten.py
@@ -4,8 +4,17 @@ Take a list, say for example this one:
 and write a program that prints out all the elements of the list that are less than 5.
 """
 
-lst = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# lst = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#
+# for i in lst:
+#     if i < 6:
+#         print(i)
 
-for i in lst:
-    if i < 6:
+# Extra
+lst2 = [1, 4, 7, 12, 15, 19, 22, 27, 33, 45, 65]
+
+input_n = int(input("Input number: "))
+
+for i in lst2:
+    if i < input_n:
         print(i)
