15c9d988f68b5bd2e4d902ee3fd8f4ab97b5c350
8bitjustin <j.olson.digital@gmail.com>
2019-09-30 14:37:55 -0700

    Created while loop for number of players.

--- 08_rock_paper_scissors.py
+++ 08_rock_paper_scissors.py
@@ -10,3 +10,17 @@ Scissors beats paper
 Paper beats rock
 """
 
+game = True
+
+while game:
+    player_input = int(input("One or two players: "))
+    if player_input == 1:
+        print("One player")
+        game = False
+    elif player_input == 2:
+        print("Two players")
+        game = False
+    else:
+        print("Please enter one or two players")
+        continue
+
