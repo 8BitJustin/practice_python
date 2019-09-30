708f7f1db0f1b8f78aaed33661a9484487c37eef
8bitjustin <j.olson.digital@gmail.com>
2019-09-30 10:31:57 -0700

    Finished 07.

--- 07_list_comprehensions.py
+++ 07_list_comprehensions.py
@@ -6,6 +6,9 @@ this list in it.
 
 a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
-b = map(lambda x: x % 2 == 0, a)
+# using list comprehension. within the new list, the first item is the expression, which ties
+# into the for loop, and then finally the if statement. If true, only even numbers are placed
+# within the new b list
+b = [num for num in a if num % 2 == 0]
 
 print(b)
