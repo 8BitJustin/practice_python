f1a879293ab0d5813718ad3355f4b7771fca4e00
8bitjustin <j.olson.digital@gmail.com>
2019-10-02 21:36:18 -0700

    Lost again, added solution to 11.

--- 11_check_primality_functions.py
+++ 11_check_primality_functions.py
@@ -5,14 +5,36 @@ to Exercise 4 to help you. Take this opportunity to practice using functions, de
 """
 
 
-def is_prime(num):
+def get_number(prompt):
+    '''Returns integer value for input. Prompt is displayed text'''
+    return int(input(prompt))
 
-    if num % 1 == num:
-        print(True)
+
+def is_prime(number):
+    '''Returns True for prime numbers, False otherwise'''
+    # Edge Cases
+    if number == 1:
+        prime = False
+    elif number == 2:
+        prime = True
+    # All other primes
     else:
-        print(False)
+        prime = True
+        for check_number in range(2, (number / 2) + 1):
+            if number % check_number == 0:
+                prime = False
+                break
+    return prime
 
-is_prime(12)
+
+def print_prime(number):
+    prime = is_prime(number)
+    if prime:
+        descriptor = ""
+    else:
+        descriptor = "not "
+    print(number, " is ", descriptor, "prime.", sep="", end="\n\n")
+
 
 """
 num = int(input("Please choose a number to divide: "))
