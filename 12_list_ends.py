dfd4517247ac76329691ac37082f74280ea048ec
8bitjustin <j.olson.digital@gmail.com>
2019-10-03 07:15:57 -0700

    Reworked 12. Turned into function, added random list generator, and displayed function with inputting list as argument instead of a variable.

--- 12_list_ends.py
+++ 12_list_ends.py
@@ -3,9 +3,23 @@ Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 2
 list of only the first and last elements of the given list. For practice, write this code inside a
 function.
 """
+import random
 
-alpha = [5, 10, 15, 20, 25]
+# makes random list
+random_list = [random.randint(0,10) for i in range(5)]
 
-beta = [alpha.pop(0), alpha.pop(-1)]
 
-print(beta)
+def list_ends(list):
+    beta = [list.pop(0), list.pop(-1)]
+
+    print(beta)
+
+
+# this prints what the random list is
+print(random_list)
+
+# this calls the function to pull the first and last items from the list
+list_ends(random_list)
+
+# this uses the function using a list instead of a variable as the argument
+list_ends([2, 4, 5, 6, 98])
